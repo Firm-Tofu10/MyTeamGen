abfc83c7c1c798e8af75fe61af375033688678ef
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-09 09:20:21 -0700

    Fixed the index a little bit
    added some functions and cleaned up the old ones

--- index.js
+++ index.js
@@ -2,34 +2,117 @@ const Manager = require("./Lib/Manager");
 const Intern = require("./Lib/Intern");
 const Engineer = require("./Lib/Engineer");
 const inquirer = require("inquirer")
-const managerList = []
+let managerList = []
+let interList = []
+let engineerList = []
 
 const boss = () => {
-   console.log("");
-   inquirer.prompt([
-    {
-        type:"input",
-        name:"employeeName",
-        message: "Enter Manager Name:  "
-    },
-    {
-        type:"input",
-        name:"employeeID",
-        message: "Enter Manager ID:  "
-    },
-    {
-        type:"input",
-        name:"eMail",
-        message: "Enter Email:  "
-    },
-    {
-        type:"input",
-        name:"officeNumber",
-        message: "Enter officeNumber:  "
-    },
-   ]).then(({employeeName, employeeID,eMail,officeNumber}) => {
-       const newHire = new Manager(employeeName, employeeID, eMail,officeNumber)
-       managerList.push(newHire);
-   })
+    console.log("");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeName",
+            message: "Enter Manager Name:  "
+        },
+        {
+            type: "input",
+            name: "employeeID",
+            message: "Enter Manager ID:  "
+        },
+        {
+            type: "input",
+            name: "eMail",
+            message: "Enter Email:  "
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter officeNumber:  "
+        },
+    ]).then(({ employeeName, employeeID, eMail, officeNumber }) => {
+        const newHire = new Manager(employeeName, employeeID, eMail, officeNumber)
+        managerList.push(newHire);
+    })
 }
 
+const question = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choices",
+            choices: ["Add Engineer", "Add Intern", "Exit App"],
+            message: ":Role Choices"
+        }
+    ]).then(({choices }) => {
+       switch(choices){
+        case "Add Engineer":
+            addEngineer();
+            break;
+        case "Add Intern":
+            addIntern();
+            break;
+        default:
+            console.log(managerList,internList,engineerList)
+            process.exit(0)
+       }
+    })
+}
+
+
+const addIntern = () => {
+    console.log("");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeName",
+            message: "Enter Intern Name",
+        },
+        {
+            type: "input",
+            name: "employeeID",
+            message: "Enter Intern ID:  "
+        },
+        {
+            type: "input",
+            name: "eMail",
+            message: "Enter Email:  "
+        },
+        {
+            type: "input",
+            name: "schoolName",
+            message: "Enter School Name:  "
+        },
+    ]).then(({ employeeName, employeeID, eMail, schoolName }) => {
+        const newHire = new Intern(employeeName, employeeID, eMail, schoolName)
+        internList.push(newHire);
+    })
+}
+
+const addEngineer = () => {
+    console.log("");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeName",
+            message: "Enter Engineer Name: "  
+        },
+        {
+            type: "input",
+            name: "employeeID",
+            message: "Enter Engineer ID:  "
+        },
+        {
+            type: "input",
+            name: "eMail",
+            message: "Enter Email:  "
+        },
+        {
+            type: "input",
+            name: "githubName",
+            message: "enterUsername:  "
+        },
+    ]).then(({ employeeName, employeeID, eMail, githubName }) => {
+        const newHire = new Engineer(employeeName, employeeID, eMail, githubName)
+        engineerList.push(newHire);
+    })
+}
