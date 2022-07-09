91d1e5b1fcf70b5f85e66023633c3a7211a95c22
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-09 10:16:43 -0700

    Fixed tests by adding more to code

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -10,3 +10,19 @@ test("Test Intern Object name attribute",function(){
     const testIntern = new Intern("Bryson",123,"test@gmail.com","Firm-Tofu10");
     expect(testIntern.name).toBe("Bryson");
   })
+
+  test("Test Intern Object ID attribute",function(){
+    const testIntern = new Intern("Bryson",123,"test@gmail.com","Firm-Tofu10");
+    expect(testIntern.name).toBe(123);
+  })
+
+  test("Test Intern Object Email attribute",function(){
+    const testIntern = new Intern("Bryson",123,"test@gmail.com","Firm-Tofu10");
+    expect(testIntern.name).toBe("test@gmail.coms");
+  })
+
+  test("Test Intern Object schoolName attribute",function(){
+    const testIntern = new Intern("Bryson",123,"test@gmail.com","schoolName");
+    expect(testIntern.name).toBe("schoolName");
+  })
+  
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -7,6 +7,21 @@ test("Test Manager Object",function(){
 })
 
 test("Test Engineer Object name attribute",function(){
-    const testManager = new Manager("Bryson",123,"test@gmail.com","Firm-Tofu10");
+    const testManager = new Manager("Bryson",123,"test@gmail.com",321);
     expect(testManager.name).toBe("Bryson");
+  })
+  
+  test("Test Engineer Object ID attribute",function(){
+    const testManager = new Manager("Bryson",123,"test@gmail.com",321);
+    expect(testManager.id).toBe(123);
+  })
+
+  test("Test Engineer Object Email attribute",function(){
+    const testManager = new Manager("Bryson",123,"test@gmail.com",321);
+    expect(testManager.Email).toBe("test@gmail.com");
+  })
+
+  test("Test Engineer Object officeNumber attribute",function(){
+    const testManager = new Manager("Bryson",123,"test@gmail.com",321);
+    expect(testManager.officeNumber).toBe(321);
   })
